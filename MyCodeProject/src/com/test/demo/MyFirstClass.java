6627cbceb41adf651122984528f14f7f70acd27c
SuryaKiran namala <suryakiran.namala@gmail.com>
2025-11-06 12:42:53 +0530

    this is my first commit code into the GIT.

---
+++ MyCodeProject/src/com/test/demo/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.test.demo;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Java- Welcome to the GIT: ");
+
+	}
+
+}
